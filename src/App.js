a71f4828deec6a95034f342c78bd7bd097b065ab
Dovran <dowran@Dowrans-MBP.lan>
2021-10-08 18:33:26 -0400

    added count with hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
-import counter from './components/counter'
+import Counter from './components/Counter'
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className='App'>
-      <h1>Hi there</h1>
-      <counter />
+      <Counter header='Counter:' count={count} setCount={setCount} />
     </div>
   )
 }
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const counter = () => {
+const counter = ({ header, count, setCount }) => {
   return (
     <div>
-      <h1>Counter:</h1>
+      <h1>
+        {header} {''} {count}
+      </h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
     </div>
   )
 }
